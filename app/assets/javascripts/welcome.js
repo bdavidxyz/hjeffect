b29dc8a458fb491faef4eecb6a45a6d055d112bf
David Boureau <bdavidxyz@gmail.com>
2018-10-11 16:04:16 +0200

    trying empty svg

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -13,3 +13,13 @@
 //     a.appendChild(r);
 //   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 // }
+
+
+window.addEventListener('load', function(){
+    var allimages= document.getElementsByTagName('img');
+    for (var i=0; i<allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false)
