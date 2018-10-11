44f9a2508e35c0d4c25461b72e4310af9b2a6690
David Boureau <bdavidxyz@gmail.com>
2018-10-11 11:49:36 +0200

    dynamic hj load

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,15 +1,15 @@
-// // Place all the behaviors and hooks related to the matching controller here.
-// // All this logic will automatically be available in application.js.
-// if (_.get(window, 'hj')) {
-//   // ok, hotjar loaded, do nothing
-// } else {
-//   //load hotjar
-//   (function(h,o,t,j,a,r){
-//     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-//     h._hjSettings={hjid:1046326,hjsv:6};
-//     a=o.getElementsByTagName('head')[0];
-//     r=o.createElement('script');r.async=1;
-//     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-//     a.appendChild(r);
-//   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-// }
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+if (_.get(window, 'hj')) {
+  // ok, hotjar loaded, do nothing
+} else {
+  //load hotjar
+  (function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1046326,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+}
